4512df4fe1a35c42b97b3d8018a4b3188185b6fa
Maks <max.polak04@gmail.com>
2025-01-08 12:43:32 +0100

    added new files

---
+++ library-project.py
@@ -0,0 +1,22 @@
+import sys
+
+if __name__ == "__main__":
+    while True:
+        print("1. Wyświetl Twoje książki")
+        print("2. Wyświetl książki w bibliotece")
+        print("3. Wypożycz książkę")
+        print("4. Zwróć książkę")
+        print("5. Zakończ program")
+
+        option = int(input("Wybierz opcję: "))
+
+        if option == 1:
+            pass
+        elif option == 2:
+            pass
+        elif option == 3:
+            pass
+        elif option == 4:
+            pass
+        elif option == 5:
+            sys.exit()
